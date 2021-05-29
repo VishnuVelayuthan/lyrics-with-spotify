2c4570c3d8417f3d7bbfd72a33f09b9826e55d36
Vishnu Velayuthan <100023569@mvla.net>
2021-05-28 22:24:59 -0700

    Started using the Spotify API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); //This is for that Cors policy thing 
 const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
 
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Evoked when expiresIn is 0 and we need a new access token
+// Uses the refresh token to get a new access token 
 app.post("/refresh", (req, res) => {
     const refreshToken = req.body.refreshToken;
     const spotifyApi = new SpotifyWebApi({
@@ -27,16 +29,18 @@ app.post("/refresh", (req, res) => {
 })
 
 
+// Evoked when first logged in to get access token from auth code
 app.post("/login", (req, res) => {
-    const code = req.body.code;
+    const code = req.body.code; // The authentication code passed from the front
     const spotifyApi = new SpotifyWebApi({
         redirectUri: "http://localhost:3000",
         clientId: "0c9eb454ffc2431cabd8ddc114204945",
         clientSecret: "82ff62c1e12949dcb8f01a9ae99a99dc"
     });
 
+    //This uses the code to give an access token
     spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
+        res.json({ //Sends the json object back to the front with all the info
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
