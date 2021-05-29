6dccae99ea3ef2049da58498a08a0d0254202855
Vishnu Velayuthan <100023569@mvla.net>
2021-05-28 20:21:19 -0700

    Got authentication working

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const spotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
-        redirectUrl: "http://localhost:3000",
+        redirectUri: "http://localhost:3000",
         clientId: "0c9eb454ffc2431cabd8ddc114204945",
         clientSecret: "82ff62c1e12949dcb8f01a9ae99a99dc"
     });
@@ -17,7 +21,10 @@ app.post("/login", (req, res) => {
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         })
-    }).catch(() => {
+    }).catch((err) => {
+        console.log(err);
         res.sendStatus(400);
     });
 });
+
+app.listen(3001); 
