870b9cd3c8e27ad299e770887c99f87319a2d593
Vishnu Velayuthan <100023569@mvla.net>
2021-05-28 23:44:15 -0700

    Got base project working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors"); //This is for that Cors policy thing 
 const bodyParser = require("body-parser");
+const lyricsFinder = require("lyrics-finder");
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Evoked when expiresIn is 0 and we need a new access token
 // Uses the refresh token to get a new access token 
@@ -49,6 +51,11 @@ app.post("/login", (req, res) => {
         console.log(err);
         res.sendStatus(400);
     });
+});
+
+app.get("/lyrics", async(req, res) => {
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || "No lyrics found";
+    res.json({lyrics});
 });
 
 app.listen(3001); 
